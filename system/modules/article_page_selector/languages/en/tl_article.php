bccb68313b65194e758d4514098fc74ec4321580
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-08-10 09:15:41 -0400

    Create tl_article.php

---
+++ system/modules/article_page_selector/languages/en/tl_article.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Article Page Selector
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/article-page-selector
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_article']['selected_page']          = array('Selected Page', 'The page selected to be used to wrap the article in a link.');
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_user']['article_page_selector_legend'] = 'Article Page Selector\'s Settings';
