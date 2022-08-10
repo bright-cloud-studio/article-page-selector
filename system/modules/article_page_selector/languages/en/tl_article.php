56fc8b86fa418dc3251449430ebed5a164e2fdf0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-08-10 09:25:49 -0400

    Update tl_article.php

--- system/modules/article_page_selector/languages/en/tl_article.php
+++ system/modules/article_page_selector/languages/en/tl_article.php
@@ -12,7 +12,7 @@
 
 
 /* Fields */
-$GLOBALS['TL_LANG']['tl_article']['selected_page']          = array('Selected Page', 'The page selected to be used to wrap the article in a link.');
+$GLOBALS['TL_LANG']['tl_article']['selected_page']          = array('Selected Page', 'Select a page to wrap the entire article in a link to it.');
 
 /* Legends */
-$GLOBALS['TL_LANG']['tl_user']['article_page_selector_legend'] = 'Article Page Selector\'s Settings';
+$GLOBALS['TL_LANG']['tl_article']['article_page_selector_legend'] = 'Article Page Selector Settings';
